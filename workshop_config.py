60aa2269e6d8dbe50d23a69a364cc3aad8deae2e
jesseryoung-db <jesseryoung-db>
2025-07-14 01:17:33 +0000

    Initial commig

---
+++ workshop_config.py
@@ -0,0 +1,13 @@
+from databricks.sdk.runtime import dbutils, spark
+import os
+
+WORKSHOP_CATALOG = "jy_workshop"
+WORKSHOP_SCHEMA = "agents"
+USER_SCHEMA = spark.sql("select regexp_replace(lower(split(current_user(), '@')[0]), '[ .]', '_')").collect()[0][0]
+
+dbutils.widgets.text("WORKSHOP_CATALOG", WORKSHOP_CATALOG)
+dbutils.widgets.text("WORKSHOP_SCHEMA", WORKSHOP_SCHEMA)
+dbutils.widgets.text("USER_SCHEMA", USER_SCHEMA)
+os.environ["WORKSHOP_CATALOG"] = WORKSHOP_CATALOG
+os.environ["WORKSHOP_SCHEMA"] = WORKSHOP_SCHEMA
+os.environ["USER_SCHEMA"] = USER_SCHEMA
